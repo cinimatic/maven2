b363e7369ed683f4a10a598f463d3a50fe283767
cinimatic <cinimatic@gmail.com>
2021-12-03 15:14:04 +0300

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -2,11 +2,11 @@ package com.techprimers.testing;
 
 public class FizzBuzz {
 
-    public static void main(int number) {
+    public static void main(String[] args) {
 
-        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
+       // if (number == 0) throw new IllegalArgumentException("Number must not be 0");
+        //if (number % 3 == 0) return "Fizz";
+        //if (number % 5 == 0) return "Buzz";
         
         System.out.println("Hello world!");
         
